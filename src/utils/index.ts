03d0fb629ce6ff2fd06f36bf3e6e66991b439ae2
Josh Hills <joshua.hills@telent.com>
2023-04-01 22:33:09 +0100

    chore: remove unused spinner class

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,4 +5,3 @@ export * from './copy.js';
 export * from './to-valid-package-name.js';
 export * from './is-valid-package-name.js';
 export * from './format-target-directory.js';
-export * from './spinner.js';
