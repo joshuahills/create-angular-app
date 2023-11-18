8571d0511cacec7d7c1459cd685cd0038a473c6f
Josh Hills <joshua.hills@telent.com>
2023-11-18 00:14:57 +0000

    chore: convert hardcoded v15 replace regex to any digit so replacements are done in v16 & v17 templates

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ const APPS = [
     ],
   },
   {
-    name: 'Angular V17',
+    name: 'Angular V17 (standalone)',
     color: red,
     variants: [
       {
@@ -198,18 +198,15 @@ async function init () {
 
   write('package.json', JSON.stringify(pkg, null, 2));
 
-  const replaceResult = replace.sync({
+  replace.sync({
     files: [
       path.join(targetDir, 'src/app/app.component.ts'),
       path.join(targetDir, 'angular.json'),
       path.join(targetDir, 'src/index.html'),
     ],
-    from: /angular-v15-template/g,
+    from:/angular-v(\d+)-template/g,
     to: pkg.name,
-    countMatches: true,
   });
-
-  console.log(gray(`Replaced ${replaceResult.reduce((a, b) => a + (b.numMatches ?? 0), 0)} template name occurrences with "${pkg.name}"`));
 
   const spinner = new Spinner('Installing dependencies...').start();
 
